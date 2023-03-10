72f12d242ad37deb83072e45b7c811d335975035
Iov Mihai <97.mihairazvan@gmail.com>
2023-03-10 18:43:37 +0200

    test

--- sample.js
+++ sample.js
@@ -1,14 +1,15 @@
+// import * as THREE from 'three'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+// const loader = new GLTFLoader()
+// let cube
+// loader.load('https://testenv.ro/files/cub.gltf', (gltfScene) => {
+//   cube = gltfScene.scene
+// })
+
 const scene = new THREE.Scene();
-
-// Create a new camera for the raycaster
-const raycasterCamera = new THREE.PerspectiveCamera();
-raycasterCamera.position.set(0, 0, 0);
-raycasterCamera.lookAt(scene.position);
-
-let mouse = new THREE.Vector2();
-let intersects = [];
-
-let raycaster = new THREE.Raycaster();
+const camera = new THREE.Camera();
+scene.add(camera)
 const renderer = new THREE.WebGLRenderer({
   antialias: true,
   alpha: true
@@ -19,9 +20,6 @@ document.body.appendChild( renderer.domElement );
 const ArToolkitSource = new THREEx.ArToolkitSource({
   sourceType: "webcam",
 })
-
-// Add an event listener for clicks
-window.addEventListener('mousedown', onMouseDown, false);
 
 ArToolkitSource.init(function() {
   setTimeout(() => {
@@ -36,13 +34,10 @@ const ArToolkitContext = new THREEx.ArToolkitContext({
 })
 
 ArToolkitContext.init(function(){
-
-  // Set the properties of the new camera to match the ARToolkitContext camera
-  raycasterCamera.projectionMatrix.copy(ArToolkitContext.getProjectionMatrix());
-  // raycasterCamera.matrixWorldInverse.fromArray(ArToolkitContext.getCameraMatrix());
+  camera.projectionMatrix.copy(ArToolkitContext.getProjectionMatrix())
 })
 
-const ArMarkerControls = new THREEx.ArMarkerControls(ArToolkitContext, raycasterCamera, {
+const ArMarkerControls = new THREEx.ArMarkerControls(ArToolkitContext, camera, {
   type: 'pattern',
   patternUrl: '/pattern.patt',
   changeMatrixMode: 'cameraTransformMatrix'
@@ -61,32 +56,14 @@ const cube = new THREE.Mesh( geometry, material );
 //   cube.position.y = geometry.parameters.height / 2
 scene.add(cube);
 // },2000)
-cube.addEventListener('click', () => {
-  console.log('Cube clicked');
-  window.open('https://www.google.com/', '_blank');
 
-});
-function onMouseDown(event) {
-  event.preventDefault();
-
-  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-
-  raycaster.setFromCamera(mouse, raycasterCamera);
-  intersects = raycaster.intersectObjects(scene.children, true);
-
-  if (intersects.length > 0 && intersects[0].object === cube) {
-    console.log(intersects)
-    console.log('Clicked on object:', intersects[0].object);
-  }
-}
 function animate() {
   requestAnimationFrame( animate );
 
   ArToolkitContext.update(ArToolkitSource.domElement)
-  scene.visible = raycasterCamera.visible
+  scene.visible = camera.visible
 
-  renderer.render( scene, raycasterCamera );
+  renderer.render( scene, camera );
 }
 
 animate();
